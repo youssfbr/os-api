a8f0fa5068e0ae712e78e776b3d5eff3df5a7994
alisson youssf <youssfbr@gmail.com>
2020-10-20 14:42:13 -0300

    project created. ClientController

---
+++ backend/src/test/java/com/alissondev/os/OsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alissondev.os;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
