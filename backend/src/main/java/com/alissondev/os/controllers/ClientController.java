5787358c6a08cb0417c58f6559f381429895924e
alisson youssf <youssfbr@gmail.com>
2020-10-21 18:58:12 -0300

    Client CRUD, validation

--- backend/src/main/java/com/alissondev/os/controllers/ClientController.java
+++ backend/src/main/java/com/alissondev/os/controllers/ClientController.java
@@ -1,10 +1,21 @@
 package com.alissondev.os.controllers;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alissondev.os.entities.Client;
@@ -20,5 +31,47 @@ public class ClientController {
 	@GetMapping
 	public  List<Client> findAll() {
 		return repository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Client> findById(@PathVariable Long id) {
+		Optional<Client> result = repository.findById(id);		
+		
+		if (result.isPresent()) {
+			return ResponseEntity.ok(result.get());
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Client insert(@Valid @RequestBody Client client) {
+		return repository.save(client);	
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Client> update(@PathVariable Long id, @Valid @RequestBody Client client) {
+		
+		if (!repository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		client.setId(id);
+		client = repository.save(client);
+		
+		return ResponseEntity.ok(client);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		
+		if (!repository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		repository.deleteById(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
--- backend/src/main/java/com/alissondev/os/entities/Client.java
+++ backend/src/main/java/com/alissondev/os/entities/Client.java
@@ -6,6 +6,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 
 @Entity
 public class Client implements Serializable {
@@ -14,8 +18,18 @@ public class Client implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank
+	@Size(max = 60)
 	private String name;
+	
+	@NotBlank
+	@Email
+	@Size(max = 255)
 	private String email;
+	
+	@NotBlank
+	@Size(max = 20)
 	private String telephone;
 	
 	public Client() {
