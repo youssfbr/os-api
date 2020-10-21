c99a940775d4a556e9d01df74d486db0972877f0
alisson youssf <youssfbr@gmail.com>
2020-10-21 12:14:24 -0300

    Dependencies, Connection MySQL, Firts migration

--- backend/src/main/java/com/alissondev/os/entities/Client.java
+++ backend/src/main/java/com/alissondev/os/entities/Client.java
@@ -2,9 +2,14 @@ package com.alissondev.os.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Client implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+		
 	private Long id;
 	private String name;
 	private String email;
